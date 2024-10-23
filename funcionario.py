3faa93a11a98aab81480720a3d40f54a511da9e4
Nathália Santos <nathmarcalsantos@gmail.com>
2024-10-23 08:43:27 -0300

    funcionario.py

--- funcionario.py
+++ funcionario.py
@@ -1,1 +1,20 @@
+class Funcionario:
+def__init__(self,nome,email):
+  self.nome=nome
+  self.email=email
+  self.horas= {}
+  self.salario_hora= {}
+defcadastro_hora(self,mes,horas):
+  if(mesnotinself.horas):
+  self.horas[mes]=horas
+defcadastro_salario_hora(self,mes,valor):
+  if(mesnotinself.salario_hora):
+  self.salario_hora[mes]=valor
+defcalcula_salario(self,mes):
+  if(mesnotinself.horas)or(mesnotinself.salario_hora):
+  print(‘Mês Inexistente!!’)
+  else:
+return self.horas[mes]*self.salario_hora[mes]
+def__repr__(self):
+return f’Funcionário:{self.nome}, \nEmail: {self.email},\nhoras/mês: {self.horas}, \nsalário-hora: {self.salario_hora}’
 
